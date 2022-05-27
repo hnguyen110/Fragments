5588f8bd6c502e986aaf4a654944277595441b13
Hien Nguyen <hnguyen110.akc@gmail.com>
2022-05-27 12:59:29 -0400

    Fix index.js for CI

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const logger = require('./logger');
+const logger = require("./logger");
 
-process.on('uncaughtException', (err, origin) => {
-  logger.fatal({ err, origin }, 'uncaughtException');
-  throw err;
+process.on("uncaughtException", (err, origin) => {
+    logger.fatal({err, origin}, "uncaughtException");
+    throw err;
 });
 
-process.on('unhandledRejection', (reason, promise) => {
-  logger.fatal({ reason, promise }, 'unhandledRejection');
-  throw reason;
+process.on("unhandledRejection", (reason, promise) => {
+    logger.fatal({reason, promise}, "unhandledRejection");
+    throw reason;
 });
 
-require('./server');
-
-const unneededVariable = 'This variable is never used';
+require("./server");
