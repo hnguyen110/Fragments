4e16a837be2c1a3c0e9131170d365eec3de0924a
Hien Nguyen <hnguyen110.akc@gmail.com>
2022-06-02 20:55:20 -0400

    Updated the get.test.js file

--- tests/unit/get.test.js
+++ tests/unit/get.test.js
@@ -1,4 +1,5 @@
 const request = require("supertest");
+const _ = require("lodash");
 
 const app = require("../../src/app");
 
@@ -8,10 +9,44 @@ describe("GET /v1/fragments", () => {
     test("incorrect credentials are denied", () =>
         request(app).get("/v1/fragments").auth("invalid@email.com", "incorrect_password").expect(401));
 
-    test("authenticated users get a fragments array", async () => {
+    test("authenticated users get an empty fragments array", async () => {
         const res = await request(app).get("/v1/fragments").auth("user1@email.com", "password1");
         expect(res.statusCode).toBe(200);
         expect(res.body.status).toBe("ok");
         expect(Array.isArray(res.body.fragments)).toBe(true);
+        expect(Object.is(JSON.stringify(res.body.fragments), JSON.stringify([]))).toBe(true);
+    });
+
+    test("authenticated users get an fragments array (ids)", async () => {
+        let response;
+        const ids = [];
+
+        response = await request(app)
+            .post("/v1/fragments")
+            .auth("user1@email.com", "password1")
+            .set("Content-Type", "text/plain")
+            .send("Hello the world");
+        ids.push(response.body.fragment.id);
+
+        response = await request(app)
+            .post("/v1/fragments")
+            .auth("user1@email.com", "password1")
+            .set("Content-Type", "text/plain")
+            .send("Hello the world");
+        ids.push(response.body.fragment.id);
+
+        response = await request(app)
+            .post("/v1/fragments")
+            .auth("user1@email.com", "password1")
+            .set("Content-Type", "text/plain")
+            .send("Hello the world");
+        ids.push(response.body.fragment.id);
+
+        response = await request(app).get("/v1/fragments").auth("user1@email.com", "password1");
+        expect(response.statusCode).toBe(200);
+        expect(response.body.status).toBe("ok");
+        expect(Array.isArray(response.body.fragments)).toBe(true);
+        expect(_.isEqual(ids.sort(), response.body.fragments.sort())).toBe(true);
+        expect(response.body.fragments.length).toBe(3);
     });
 });
